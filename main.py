ed7ddc15ad145a0f5de6e0f161e4a82e6a940783
Whitney Goodey <wgoodey@gmail.com>
2022-12-08 10:43:10 -0800

    Added stripping and filtering to extract names in files with inconsistent formatting.

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ OUTPUT_DIRECTORY = "./Output/ReadyToSend"
 
 def process_names_list(list_path):
     with open(list_path) as file:
-        name_list = file.read().splitlines()
-        return name_list
+        name_list = file.readlines()
+        for recipient in range(len(name_list)):
+            name_list[recipient] = name_list[recipient].strip()
+        # filter out empty items and return list
+        return list(filter(None, name_list))
 
 
 def insert_name(recipient, text_path):
@@ -18,7 +21,7 @@ def insert_name(recipient, text_path):
 
 def write_file(recipient, letter_text, output_directory):
     underscored_name = recipient.replace(' ', '_')
-    with open(f"{output_directory}/{underscored_name}_letter.txt", "w") as file:
+    with open(f"{output_directory}/letter_for_{underscored_name}.txt", mode="w") as file:
         file.write(letter_text)
 
 
@@ -28,19 +31,19 @@ def create_letter(recipient, letter_to_path, output_directory):
 
 
 # Using hardcoded file paths
-# names = process_names_list(NAMES_PATH)
-# for name in names:
-#     create_letter(name, LETTER_PATH, OUTPUT_DIRECTORY)
-
-
-# Using user input for file paths
-names_path = input("What is the file path of the invite list?\n")
-letter_path = input("What is the file path of the starting letter?\n")
-output_directory_path = input("Where do you want to save the letters?\n")
-
-names = process_names_list(names_path)
-
+names = process_names_list(NAMES_PATH)
 for name in names:
-    create_letter(name, letter_path, output_directory_path)
+    create_letter(name, LETTER_PATH, OUTPUT_DIRECTORY)
 
-print("Finished!")
+
+# # Using user input for file paths
+# names_path = input("What is the file path of the invite list?\n")
+# letter_path = input("What is the file path of the starting letter?\n")
+# output_directory_path = input("Where do you want to save the letters?\n")
+#
+# names = process_names_list(names_path)
+#
+# for name in names:
+#     create_letter(name, letter_path, output_directory_path)
+#
+# print("Finished!")
